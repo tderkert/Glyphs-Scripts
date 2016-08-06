db4605453eba077718f1249d20a11da14acfa5da
Teddy Derkert <teddyderkert@gmail.com>
2016-08-06 17:38:57 +0200

    Add 'New Tab with kerning pairs for F,T,V,W,Y to selected glyphs' script

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Python scripts for Glyphsapp
 
 ## Scripts
 
-**Fixed Width and Center Selected Glyphs:** Set fixed width and center selected glyphs. (Proceed with caution as it decomposes all components to avoid weird positioning)
+**New Tab with kerning pairs for F,T,V,W,Y to selected glyphs:** Useful for making kerning exceptions for tall diacritical characters, i.e. 'Tȁ'  
+**Fixed Width and Center Selected Glyphs:** Set fixed width and center selected glyphs. (Proceed with caution as it decomposes all components to avoid weird positioning)  
---
+++ new-tab-with-kerning-pairs-for-FTVWY-to-selected-glyphs.py
@@ -0,0 +1,27 @@
+#MenuTitle: New Tab with kerning pairs for F,T,V,W,Y to selected glyphs
+'''Useful for making kerning exceptions for tall diacritical characters'''
+
+font = Glyphs.font
+selectedGlyphs = font.selection
+
+
+masterLetters = "FTVWY"
+sideLetters = selectedGlyphs
+prepend = "HHOH"
+append = "nnoi"
+finalString = ""
+
+for mLetter in masterLetters:
+	for sLetter in sideLetters:
+		sLetterString = '/'+sLetter.name+' '
+
+		newLine = prepend
+		# newLine += sLetterString
+		newLine += mLetter
+		newLine += sLetterString
+		newLine += append
+		finalString += newLine+'\n'
+
+font.newTab(finalString)
+font.currentTab.textCursor(0)
+print(finalString)
