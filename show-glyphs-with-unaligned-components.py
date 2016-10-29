a770bd6cdf6fa86adf6f0e2e230551d8921b479e
Teddy Derkert <teddyderkert@gmail.com>
2016-10-29 16:24:25 +0200

    Add 'show-glyphs-with-unaligned-components.py'

---
+++ show-glyphs-with-unaligned-components.py
@@ -0,0 +1,22 @@
+#MenuTitle: New tab with glyphs containing unaligned components
+'''Open new tab with glyphs containing unaligned components (only in current layer)'''
+
+font = Glyphs.font
+glyphs = font.glyphs
+glyphsString = ''
+
+print("Glyphs with unaligned components:")
+for glyph in glyphs:
+	layer = glyph.layers[0]
+	components = layer.components
+	hasUnalignedComponent = False
+	
+	for component in components:
+		if component.automaticAlignment == False:
+			glyphsString = glyphsString + "/" + glyph.name
+			print("/" + glyph.name)
+			break
+
+
+# Open new tab
+thisFont.newTab( glyphsString )
