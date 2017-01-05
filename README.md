8b252bdb44c553c6f7eddafa1873996d87afc343
Teddy Derkert <teddyderkert@gmail.com>
2017-01-05 11:25:44 +0100

    Change to Wordnik API for random words.

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Python scripts for [Glyphs](glyphsapp.com) font editor.
 * **New tab with glyphs containing bracket layers:** Open new tab with glyphs containing bracket layers.  
 * **New tab with glyphs containing unaligned components:** Open new tab with glyphs containing unaligned components (only in current layer)  
 * **New Tab With Pangram from Selected Langauge:** Select a language in list and open a new tab with a pangram.(~200 pangrams) (Source: [clagnut](http://clagnut.com/blog/2380/))  
-* **New Tab With Random Words:** New tab with 10 random words from 'http://www.setgetgo.com/randomword' API. (Required internet connection)  
+* **New Tab With Random Words:** New tab with 10 random words from 'http://www.wordnik.com' API. (internet connection required)  
 * **Print An Array Of All Unicode Glyphs In String Format**  
 * **Sync All Bracket Layer Metrics With Parent Layer:** Copies metrics from master layer to its associated bracket layers.  
--- new-tab-with-random-words.py
+++ new-tab-with-random-words.py
@@ -1,23 +1,28 @@
 #MenuTitle: New Tab With Random Words
 # -*- coding: utf-8 -*-
 __doc__="""
-New tab with 10 random words from 'http://www.setgetgo.com/randomword' API. (Required internet connection)
+New tab with 10 random words from 'http://www.wordnik.com' API. (internet connection required)
 """
 
 import urllib2
+import json
 
 font = Glyphs.font
-url = "http://www.setgetgo.com/randomword/get.php"
-length = 10
 tabString = ""
+apiUrl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=3191606ee718b5a4702020562100443d08ef308204146b103"
+wordList = getWords(apiUrl)
 
-def getPage(url):
+# Create string of words
+for word in wordList:
+	tabString = tabString + word["word"] + "\n"
+
+# Open tab with words
+font.newTab(tabString)
+
+# Get words from url
+def getWords(url):
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
-	return response.read()
+	return json.loads(response.read())
 
-for i in range(length):
-	tabString = tabString + getPage(url) + '\n'
 
-if isinstance(tabString, basestring):
-	font.newTab(tabString)
