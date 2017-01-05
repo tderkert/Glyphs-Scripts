26e9472495d1509cf290f77addeb60f16599b5ca
Teddy Derkert <teddyderkert@gmail.com>
2017-01-05 13:10:49 +0100

    Fix bug

--- new-tab-with-random-words.py
+++ new-tab-with-random-words.py
@@ -10,6 +10,14 @@ import json
 font = Glyphs.font
 tabString = ""
 apiUrl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=3191606ee718b5a4702020562100443d08ef308204146b103"
+
+# Get words from url
+def getWords(url):
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	return json.loads(response.read())
+
+# Word list
 wordList = getWords(apiUrl)
 
 # Create string of words
@@ -18,11 +26,3 @@ for word in wordList:
 
 # Open tab with words
 font.newTab(tabString)
-
-# Get words from url
-def getWords(url):
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
-	return json.loads(response.read())
-
-
