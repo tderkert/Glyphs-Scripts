92199d55264ec11541d501c11d631d37a4fca4a1
Teddy Derkert <teddyderkert@gmail.com>
2017-01-30 11:54:51 +0100

    Limit word pool in API request

--- new-tab-with-random-words.py
+++ new-tab-with-random-words.py
@@ -9,7 +9,7 @@ import json
 
 font = Glyphs.font
 tabString = ""
-apiUrl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=3191606ee718b5a4702020562100443d08ef308204146b103"
+apiUrl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=noun,adjective,verb,adverb,pronoun,prepostition,abbreviation,auxiliary-verb,conjunction,proper-noun&minCorpusCount=4&maxCorpusCount=-1&minDictionaryCount=10&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=3191606ee718b5a4702020562100443d08ef308204146b103"
 
 # Get words from url
 def getWords(url):
